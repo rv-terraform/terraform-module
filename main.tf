c9a7207a0d6391312769098334cde8c2b8eea2c0
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 14:18:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module mycompute {
     ssh_key = "~/.ssh/id_rsa.pub"
     vm_os_simple = "CentOS"
     remote_port = "22"
-    nb_instances = 2
+    nb_instances = 1
     public_ip_address_allocation = "dynamic"
     public_ip_dns = ["venerari123"]
     vnet_subnet_id = "${module.network.vnet_subnets[0]}"
