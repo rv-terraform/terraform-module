6a4d1ad7fe66b3ae8857240de701c9667aaaff7b
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 09:58:17 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+module mycompute {
+    source = "Azure/compute/azurerm"
+    resource_group_name = "myResourceGroup"
+    location = "Canada East"
+    admin_password = "Thepassword123"
+    vm_os_simple = "CentOS"
+    remote_port = "22"
+    nb_instances = 2
+    public_ip_dns = ["venerari123"]
+    vnet_subnet_id = "${module.network.vnet_subnets[0]}"
+}
+
+module "network" {
+    source = "Azure/network/azurerm"
+    location = "Canada East"
+    resource_group_name = "myResourceGroup"
+}
+
+output "vm_public_name" {
+    value = "${module.mycompute.public_ip_dns_name}"
+}
+
+output "vm_public_ip" {
+    value = "${module.mycompute.public_ip_address}"
+}
+
+output "vm_private_ips" {
+    value = "${module.mycompute.network_interface_private_ip}"
+}
