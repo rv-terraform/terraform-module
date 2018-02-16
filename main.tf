b6a6269fcb9aad7b1ce499b1edc9731dba6b7981
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 12:03:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,14 @@ module mycompute {
     resource_group_name = "${var.resource_group_name}"
     location = "${var.location}"
     admin_username = "${var.adminuser}"
-    admin_password = "${var.adminpassword}"
+    ssh_key = "~/.ssh/id_rsa.pub"
     vm_os_simple = "CentOS"
     remote_port = "22"
-    nb_instances = 2
+    nb_instances = 1
+    public_ip_address_allocation = "dynamic"
     public_ip_dns = ["venerari123"]
     vnet_subnet_id = "${module.network.vnet_subnets[0]}"
+    storage_account_type = "Standard_LRS"
 }
 
 module "network" {
